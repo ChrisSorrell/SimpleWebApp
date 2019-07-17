59fcd28a7c6fb73e2cefec86ea29271f8b73465f
Chris Sorrell <chris.sorrell@newportcredentialing.com>
2019-07-16 23:25:54 -0400

    Add project files.

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SimpleWebApp.Models;
+
+namespace SimpleWebApp.Controllers
+{
+    public class AccountController : Controller
+    {     
+        [HttpPost]        
+        public ActionResult LogOn(LogOnModel model)
+        {
+            var charArray = model.UserName.ToCharArray();
+            Array.Reverse(charArray);
+            var reversedUsername = new string(charArray);
+            if (reversedUsername == model.Password) {
+                Session["User"] = model.UserName;
+                return Json(new { Success = true });
+            }
+            return Json(new { Success = false });
+
+        }
+
+        public ActionResult LogOff()
+        {
+            Session.Remove("User");
+            Session.Contents.RemoveAll();
+            Session.RemoveAll();
+
+            return Json(new { Success = true });
+        }
+
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleWebApp.Models;
+
+namespace SimpleWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {            
+            return View();
+        }
+
+        public ActionResult Contact()
+        {           
+            return View();
+        }
+        
+    }
+}
